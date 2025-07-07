b05044e812a093755fe8e4f2ca04339777630e01
Gabriel Moretti <bielmoreti@gmail.com>
2025-07-07 01:00:53 -0300

    feat: adding storage cart provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
-import { CartProvider } from './contexts/CartContext';
+import { CartProvider, useCart } from './contexts/CartContext';
 import MainLayout from './components/Layout/MainLayout';
 import HomePage from './pages/HomePage/HomePage';
 import LoginPage from './pages/Auth/LoginPage';
@@ -11,24 +11,36 @@ import ProductsPage from './pages/Products/ProductsPage';
 import OrderPage from './pages/Order/OrderPage';
 import PaymentPage from './pages/Payment/PaymentPage';
 import OrdersPage from './pages/Orders/OrdersPage';
+import CartSidebar from './components/CartSidebar/CartSidebar';
+
+const AppContent: React.FC = () => {
+  const [cartOpen, setCartOpen] = useState(false);
+  const { totalItems } = useCart();
+
+  return (
+    <>
+      <MainLayout onCartClick={() => setCartOpen(true)} cartCount={totalItems} />
+      <CartSidebar open={cartOpen} onClose={() => setCartOpen(false)} />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/products/:category" element={<ProductsPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path='/order' element={<OrderPage />} />
+        <Route path='/payment/:orderId' element={<PaymentPage />} />
+        <Route path='/orders' element={<OrdersPage />} />
+      </Routes>
+    </>
+  );
+};
 
 const App: React.FC = () => {
   return (
     <Router>
       <AuthProvider>
         <CartProvider>
-          <MainLayout>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/signup" element={<SignupPage />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/products/:category" element={<ProductsPage />} />
-              <Route path="/cart" element={<CartPage />} />
-              <Route path='/order' element={<OrderPage />} />
-              <Route path='/payment/:orderId' element={<PaymentPage />} />
-              <Route path='/orders' element={<OrdersPage />} />
-            </Routes>
-          </MainLayout>
+          <AppContent />
         </CartProvider>
       </AuthProvider>
     </Router>
